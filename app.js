42204f69df6a1696fcf5978e3435e8a501875b0a
Hamees Ahmed Khan <hamees.a@allshorestaffing.com>
2022-07-07 22:25:38 +0500

    Added auth example in the skeleton

--- app.js
+++ app.js
@@ -1,11 +1,67 @@
-const express = require('express')
-const app = express()
-const port = 3000
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var session = require('express-session');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var authRouter = require('./routes/auth');
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// middleware
+
+app.use(express.urlencoded({ extended: false }))
+app.use(session({
+  resave: false, // don't save session if unmodified
+  saveUninitialized: false, // don't create session until something stored
+  secret: 'shhhh, very secret'
+}));
+
+// Session-persisted message middleware
+
+app.use(function (req, res, next) {
+  var err = req.session.error;
+  var msg = req.session.success;
+  delete req.session.error;
+  delete req.session.success;
+  res.locals.message = '';
+  if (err) res.locals.message = 'p.msg.error ' + err;
+  if (msg) res.locals.message = 'p.msg.success ' + msg;
+  next();
+});
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/auth', authRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ routes/users.js
@@ -0,0 +1,19 @@
+
+
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+  res.send('respond with a resource');
+});
+
+// app.put('/user', (req, res) => {
+//   res.send('Got a PUT request at /user')
+// })
+
+// app.delete('/user', (req, res) => {
+//   res.send('Got a DELETE request at /user')
+// })
+
+module.exports = router;
